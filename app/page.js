e19fc57789d10439ca3ebcb4baec839a6764c2ed
aryan-finix-amin <aryanfinixamin1104@gmail.com>
2024-07-16 11:57:02 -0600

    landing page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,21 @@
+import React from "react";
 import Image from "next/image";
+import Logo from "../public/logo.jpg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-white">
-      <div className="text-2xl font-bold bg-orange-200 h-10 w-full m-3">
-        <h1 className="ml-10 mt-0.5">ShopHub</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between bg-orange-100">
+      <div className="text-2xl font-bold bg-amber-300 h-20 w-full m-3 shadow-md">
+        <div className="flex items-center ml-5">
+          <Image
+            src={Logo}
+            alt="Logo"
+            height={40}
+            width={40}
+            className="m-5 rounded-lg"
+          />
+          <h1 className="">ShopHub</h1>
+        </div>
       </div>
     </main>
   );
