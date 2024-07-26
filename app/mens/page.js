187c275acad62bc920a57d8f4230a307c69ce1df
39ansh <141595198+39ansh@users.noreply.github.com>
2024-07-26 13:38:13 -0600

    men commit

--- app/mens/page.js
+++ app/mens/page.js
@@ -1,5 +1,13 @@
 import React from "react";
+import Header from "../components/header";
+import Footer from "../components/footer";
 
-export default function page() {
-  return <>cvxhjvjbv</>;
+export default function Men() {
+  return (
+    <>
+    <Header/>
+    <Footer/>
+    </>
+  )
+
 }
